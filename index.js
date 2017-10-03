823f3eff01d84c954929a05e95d4062625b86851
drshariqalikhan <31785258+drshariqalikhan@users.noreply.github.com>
2017-10-03 18:01:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+//express post data
+//import modules
+const ex =require('express');
+const bp = require('body-parser');
 
+
+//configure imports
+const app = ex();
+app.use(bp.urlencoded({extended: true}));
+
+app.post('/user',function(req,res){
+	console.log(req.body.name); 
+	//res.send('done!');
+	//console.log('done!');
+	});
